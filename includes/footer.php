afff3be78c0bbbed84ce9bf295e48e03f6419e0a
victor <victorhs.almeida131@gmail.com>
2024-08-22 01:27:03 -0300

    criação do header, footer e index

---
+++ includes/footer.php
@@ -0,0 +1,37 @@
+<body>
+    <footer id="foo"> <!--    -->
+        <div id="perguntas">
+            <h1 id="titulo">Duvidas Frequentes</h1>
+            <ul>
+                <a href="" id="linkfoo"> <li>Vocês vendem no site?        </li> </a>
+                <br>
+                <a href="" id="linkfoo"> <li>Quais são nossos produtos?</li> </a>
+                <br>
+                <a href="" id="linkfoo"> <li>Porque deveria nós avaliar?         </li> </a>
+            </ul>
+
+        </div>
+
+        <div id="logo">
+        <img src="multimidia/Logo_Foo.png" alt="logo da agro malandrin" >
+        </div>
+
+        <div id="links">   
+            <h1 id="titulo">Contate-nos</h1>
+            <table>
+                <tr>
+                    <td > <a href="https://www.facebook.com/agromalandrin/" target="_blank" id="linkfoo"> <li> FaceBook </li> </a> </td>
+                    
+                    <td> <a href="https://www.instagram.com/agromalandrin/" target="_blank" id="linkfoo"> <li> Instagram </li> </a> </td>                    
+                </tr>
+                <tr>
+                    <td> <a href="https://wa.me/551938431393"  target="_blank" id="linkfoo"> <li> Whatsapp </li> </a> </td>
+
+                    <td> <a href="">  <li id="linkfoo"> 2 </li> </a> </td>
+                </tr>
+            </table>
+        </div>
+    </footer>
+</body>
+</html>
+
---
+++ includes/header.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <!-- conectando com o css-->
+    <link href="estilos/estilo.css" rel="stylesheet" type="text/css">
+
+</head>
+ 
+<body>
+    <header id="header"><!-- criando o header -->
+        <a href="index.php"> <img src="multimidia/logo_head.png" alt="logo Agro malandrin" id="logo"> </a>
+
+        <!-- criando o nav -->
+        <div id="navbar" >
+    
+            <!-- links do nav -->
+            <a href="index.php" id="nav"> <h1>Home</h1> </a>
+            <a href="dicas.php" id="nav"> <h1>Dicas</h1> </a>  
+            <a href="Produtos.php" id="nav"> <h1>Produtos</h1> </a>  
+ 
+        </div>
+    
+        <!-- colocando o botão para poder logar -->
+       <div id="login"> <a href="" id="link"> <p>Ola, logue ou <br>cadastre-se</p> </a> </div>
+    </header>
+   
+ 
+   
