310555c839e1f9707075a71c8a5e69089fe90a24
victor <victohs.almeida131@gmail.com>
2024-10-04 11:56:41 -0300

    finalização do header mobile

--- includes/header.php
+++ includes/header.php
@@ -6,96 +6,46 @@
     
     <!-- conectando com o css-->
     <link href="estilos/estilos.css" rel="stylesheet" type="text/css">
-    
+
     <!-- colocando o icone da pagina-->
     <link rel="shortcut icon" href="multimidia/icones/planta.png" type="image/x-icon">
 
-    <script sr  c="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- linkando ao Bootstrap v5.1 -->
+        <!--CSS-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <style>
-        
-        #header #header-int{
-            /*cor do background*/
-            background-color: #034F0A;
-        }
-        
-        #header img{
-            /* editando a localização da logo da agro */
-            margin-top: -15%;
-            margin-bottom: -9.5%;
-            margin-left: 20%;
-            
-        }
-        
-        /*config navbar*/
-            
-            #header #navbar #nav{
-                /* definindo a cor e o tipo de fonte do texto dos links da navbar */
-                color: #fff;
-                font-family: arial;
-            }  
-            
-            #header #navbar a h1{
-                /* definindo o tamanho do texto dos links da navbar */
-                font-size: 1.9em;
-            }
-
-            #itens-navbar{
-                /* editando o visual dos itens da navbar  */
-                margin-right: 8%;
-                margin-left: 9%;
-                border-top: 2px solid #034F0A;
-                border-bottom: 2px solid #034F0A;
-                border-right: 2px solid #ffffff;
-                border-left: 2px solid #ffffff;
-            }
-
-            #header #navbar #nav:hover{
-                /* colocando o brilhinho quando passa o mouse em cima */
-                color: #FFF086;
-            }  
-
-        
-            #header #login{
-                /*editando o visual do butão de login*/
-                font-size: 1.2em;
-                font-family: arial;
-                margin-top: 15px;
-                margin-right: 8%;
-            }
-
-            #header #login #link {
-                /* tirando a decoração do link */
-                text-decoration: none; 
-                color:#fff;
-            }
-
-            #header #login #link:hover{
-                /* colocando o brilhinho quando passa o mouse em cima */
-                color: #FFF086;
-            }   
-
-    </style>
+    
 </head>
  
 <div id="header">
-<!-- criando o header -->
+    <!-- linkando ao Bootstrap v5.1 -->
+        <!--JAVASCRIP-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+
+    <!-- criando o header -->
+
     <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="header-int">
         <div class="container-fluid">
         
-            <!-- controla se a pagina for menor troca pela imagen menor -->
+            
             <picture>
-                <source media="(min-width: 1000px)" srcset="../multimidia/logo_Head.png">
-                <img src="../multimidia/logo_Head_mob.png" alt="logo Agro malandrin" id="logo">
+                <source media="(min-width: 992px)" srcset="multimidia/logo_Head.png">
+                <img src="multimidia/logo_Head_mob.png" alt="logo Agro malandrin" id="logo">
             </picture>
         
             
-            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-                <span class="navbar-toggler-icon"></span>
+            <button class="navbar-toggler bg-light" id="head-mob" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
+                <span class="navbar-toggler-icon bg-light" ></span>
             </button>
 
             <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             
+                <div class="offcanvas-header lex-grow-1">
+                    <h5 class="offcanvas-title" id="offcanvasNavbarLabel"> Menu </h5>
+                    
+                    <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                </div>
                 <!-- criando o nav -->
                 <div class="offcanvas-body" id="navbar">
                     <ul class="navbar-nav justify-content-evenly  flex-grow-1 pe-3" id="itens-navbar">
@@ -116,9 +66,9 @@
                     </ul>
                 
                     
-                    <form class="d-flex justify-content-around" id="login">
+                    <form class="d-flex " id="login">
                         <!-- colocando o botão para poder logar -->
-                        <a href="login.php" id="link"> <p>Ola, logue ou <br>cadastre-se</p> </a> 
+                        <a href="login.php" id="link"> <p>Ola, logue ou cadastre-se</p> </a> 
                     </form>
                     
                 </div>
